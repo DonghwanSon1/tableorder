669cbff497de32e1b9fe6f31e4c73cc74317235d
SonDongHwan <sdh549000@gmail.com>
2024-10-18 19:18:47 +0900

    Feat: aws -> 개인 노트북 서버로 이전4

---
+++ jenkins/build.window.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('git clone') {
+            steps {
+                git branch: 'main', credentialsId: 'gitHub-token', url: 'https://github.com/DonghwanSon1/tableorder.git'
+            }
+        }
+
+        stage('Docker Container Stop And Remove') {
+            steps {
+                bat 'docker rm -f table-order-service'
+            }
+        }
+
+        stage('Build and Run with Docker Compose') {
+            steps {
+                dir('./tableorder') {
+                    bat 'docker-compose up --build -d'
+                }
+            }
+        }
+
+        stage('Image Remove') {
+            steps {
+                bat 'docker image prune -f'
+            }
+        }
+    }
+}
