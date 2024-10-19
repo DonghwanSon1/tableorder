30e69c16d139422a2b3b85464b0e4ec3fd2a5a67
SonDongHwan <sdh549000@gmail.com>
2024-10-20 07:05:41 +0900

    Style: 주석 정리

--- src/main/kotlin/tableorder/tableorder/global/exception/CommonException.kt
+++ src/main/kotlin/tableorder/tableorder/global/exception/CommonException.kt
@@ -1,6 +1,5 @@
 package tableorder.tableorder.global.exception
 
-// TODO exception message가 응답을 주지 않는거 확인하기
 class CommonException(val exceptionCode: CommonExceptionCode) : RuntimeException(exceptionCode.message) {
 
 }
